3ffe747ac9cd1ed9abb28d9b7a9ac1a47ba887ef
Cuthbert20 <knowltonspencer@gmail.com>
2019-09-23 11:38:33 -0600

    first commit

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const massive = require('massive')
+require("dotenv").config();
+const {SESSION_SECRET, CONNECTION_STRING, SERVER_PORT} = process.env
+const session = require('express-session')
+const cntrl = require('./controller')
+
+app.use(express.json());
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 2
+    }
+}))
+
+massive(CONNECTION_STRING).then(db => {
+    app.set("db".db);
+    app.listen(SERVER_PORT, () => console.log(`${SERVER_PORT}, is now up and listening`))
+})
