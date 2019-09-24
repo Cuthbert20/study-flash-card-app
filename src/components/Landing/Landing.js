794fc12545aae3074fba282da4ab177c88116041
Cuthbert20 <knowltonspencer@gmail.com>
2019-09-24 10:49:27 -0600

    adding routes and landing components

---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './Landing.scss'
+import Login from '../Login/Login'
+
+export default class Landing extends Component {
+    render() {
+        return(
+            <div>
+                <Login />
+            </div>
+        )
+    }
+}
---
+++ src/components/Login/Login.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+import './Login.scss'
+
+export default class Login extends Component {
+    state = {
+
+    }
+    render() {
+        return(
+            <div>
+                <h1>Login</h1>
+                <input placeholder='username' type="text"/>
+                <input placeholder='password' type="text"/>
+                <button>Login</button>
+            </div>
+        )
+    }
+}
